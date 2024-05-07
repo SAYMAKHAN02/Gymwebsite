ddca7169dc2819d6c6037855982ed9da8c9d5d3d
SAYMAKHAN02 <141947325+SAYMAKHAN02@users.noreply.github.com>
2024-05-07 19:57:48 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,39 @@
+function home(){
+    document.getElementById("homes").style.color="red";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+}
+
+function program(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="red";
+}
+
+function plan(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="red";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+}
+
+function blog(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="red";
+    document.getElementById("programs").style.color="white";
+}
+
+function contact(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="red";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+}
